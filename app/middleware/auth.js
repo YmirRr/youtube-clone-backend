6cf5a0094b2bdabf4eef51cb6a62800578fc6a33
林晨 <136516935@qq.com>
2022-08-14 20:52:17 +0800

    获取频道信息

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -1,19 +1,19 @@
-module.exports = () => {
+module.exports = (options = { required: true }) => {
   return async (ctx, next) => {
     // 1.获取请求头中的 token 数据
-    console.log(ctx.headers['authorization'])
     let token = ctx.headers['authorization'] // Bearer空格token
     token = token ? token.split('Bearer ')[1] : null
     // 2.验证 token，无效 401
-    if (!token) {
-      ctx.throw(401)
-    }
-    ctx.token = token
-    try {
-      // 3.token 有效，根据 userId 获取用户数据挂载到 ctx 对象中给后续中间件使用
-      const data = ctx.service.user.verifyToken(token)
-      ctx.user = await ctx.model.User.findById(data.userId)
-    } catch (err) {
+    if (token) {
+      ctx.token = token
+      try {
+        // 3.token 有效，根据 userId 获取用户数据挂载到 ctx 对象中给后续中间件使用
+        const data = ctx.service.user.verifyToken(token)
+        ctx.user = await ctx.model.User.findById(data.userId)
+      } catch (err) {
+        ctx.throw(401)
+      }
+    } else if (options.required) {
       ctx.throw(401)
     }
     // 4.执行后续中间件
